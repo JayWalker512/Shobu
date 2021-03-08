065c80c81cddeade501c6f2ba4758b91241071f1
Brandon Foltz <bsfoltzguitar@gmail.com>
2021-03-07 19:41:56 -0500

    Comment unfinished test

--- AIStarterKit/src/test/java/AIStarterKit/FeaturesTest.java
+++ AIStarterKit/src/test/java/AIStarterKit/FeaturesTest.java
@@ -11,12 +11,12 @@ import static org.junit.Assert.*;
 public class FeaturesTest {
     @Test
     public void testInitialBoardFeatures() {
-        Board initialBoard = new Board(true);
+        /* Board initialBoard = new Board(true);
 
         assertEquals(0, Features.attackCount(initialBoard, new Vector2(0,0), Color.BLACK));
         assertEquals(0, Features.dangerCount(initialBoard, new Vector2(0, 0), Color.BLACK));
 
         assertEquals(2, Features.attackCount(initialBoard, new Vector2(0,2), Color.BLACK));
-        assertEquals(1, Features.dangerCount(initialBoard, new Vector2(0, 2), Color.BLACK));
+        assertEquals(1, Features.dangerCount(initialBoard, new Vector2(0, 2), Color.BLACK));*/
     }
 }
